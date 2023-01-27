58cd1be146500a85012c89032da74facea04d344
Breno Fernandes <fernandesopa@gmail.com>
2023-01-27 13:41:26 -0300

    corrige a estrutura das pastas

---
+++ terraform/pipeline-cicd/terraform-exemplo2/variables.tf
@@ -0,0 +1,13 @@
+variable "network" {
+  description = "rede"
+  type        = string
+  default     = "terraform-network"
+}
+
+variable "centro_custo_rh" {
+  description = "recursos-humanos"
+  type        = string
+  default     = "rh"
+}
+
+
